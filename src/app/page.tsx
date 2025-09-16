f4c6ed4141195882983a8e5b2e15bc6ea1fa4192
Nguyen-Son-Tung <97228470+Nguyen-Son-Tung@users.noreply.github.com>
2025-09-16 11:21:53 +0700

    Add UI Card and Button components, update layout and home page (#1)
    Introduced reusable Card and Button components in the UI library. Updated the home page to showcase a feature card using these components. Enhanced the layout with a header and main content area, and improved global styles for headings. Added @radix-ui/react-slot as a dependency.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,42 @@
+import { Button } from '@components/components/ui/button';
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+} from '@components/components/ui/card';
+import Link from 'next/link';
+
 export default function Home() {
-  return <main></main>;
+  const items = [
+    {
+      title: 'Vocabulary dictionary',
+      description:
+        'Look up new words, learn their meanings, and see example sentences to understand how to use them correctly.',
+      link: '#',
+    },
+  ];
+
+  return (
+    <main>
+      <h2 className="mb-3 font-bold">key features</h2>
+      <section className="grid gap-2 md:grid-cols-3">
+        {items.map((feature) => (
+          <Card key={feature.title} className="py-2">
+            <CardHeader className="text-zinc-600 font-semibold">
+              {feature.title}
+            </CardHeader>
+            <CardContent className="text-zinc-500 text-sm">
+              <p>{feature.description}</p>
+            </CardContent>
+            <CardFooter className="flex flex-row-reverse">
+              <Button variant="outline" size={'sm'} asChild>
+                <Link href={feature.link}>View details</Link>
+              </Button>
+            </CardFooter>
+          </Card>
+        ))}
+      </section>
+    </main>
+  );
 }
